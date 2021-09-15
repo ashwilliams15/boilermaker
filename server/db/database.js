b839bc94882cc27acad0a1f67710626e753a25a1
Ashley Williams <ashleyswilliams01@gmail.com>
2021-09-15 08:20:43 -0600

    updated databse for heroku

--- server/db/database.js
+++ server/db/database.js
@@ -1,6 +1,21 @@
-// const chalk = require('chalk');
 const Sequelize = require('sequelize');
 // const pkg = require('../../public/bundle');
+
+const config = {
+  logging: false
+}
+
+if(process.env.LOGGING === 'true') {
+  delete config.logging
+}
+
+if(process.env.DATABASE_URL) {
+  config.dialectOptions = {
+    ssl: {
+      rejectUnauthorized: false
+    }
+  }
+}
 
 const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/ingredients', {
   logging: false
